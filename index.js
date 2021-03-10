a7227cd2b9fb78bcda53f5ca57e6544b0ebb050c
shaunvan119 <shaunvan@live.com.au>
2021-03-10 18:01:15 +1100

    Completed assignment

---
+++ index.js
@@ -0,0 +1,33 @@
+
+function introduction(name){
+    return `Hi, my name is ${name}.`
+}
+
+
+introduction(Josh)
+
+function introductionWithLanguage(name, language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
+
+
+
+introductionWithLanguage(Josh, Ember.js)
+
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+} 
+
+
+
+introductionWithLanguageOptional(Gracie)
+
+
+
+
+
+
+
+
+
+ 
